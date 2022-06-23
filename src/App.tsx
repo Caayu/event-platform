b7489d19ee82be396eaaa6d035f803bf6a330e6a
Caayu <henrycay@outlook.com>
2022-06-23 16:05:31 -0300

    Aula 02

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-function App() {
+import { Event } from './pages/Event'
+
+export function App() {
   return (
-    <h1 className="text-5xl font-bold text-violet-500">Hello World!</h1>
+    <Event />
   )
 }
-
-export default App
